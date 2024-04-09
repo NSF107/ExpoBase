9e65e5b555fd2d4d20082afa907766d3ba804aea
Nati <fettsoccer10@gmail.com>
2024-04-09 16:09:13 -0500

    convert sign out button to a pressable component

--- src/components/auth/SignOutButton.tsx
+++ src/components/auth/SignOutButton.tsx
@@ -1,11 +1,33 @@
-import { Button } from "react-native";
+import { Pressable, Text, StyleSheet } from 'react-native';
 import { signOut } from "@/config/authHandler";
 import { useRouter } from 'expo-router';
 
-export default function SignOutButton() {
+export default function SignOutButton(props: any) {
     const router = useRouter();
+    const { onPress, title = 'Sign Out' } = props;
   
     return (
-        <Button onPress={() => signOut(router)} title="Sign out" />
+        <Pressable style={styles.button} onPress={() => signOut(router)}>
+            <Text style={styles.text}>{title}</Text>
+        </Pressable>
     );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});
