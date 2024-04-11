0f46b1fa3f9f102f4180b0c7c69038972522c153
Nati <fettsoccer10@gmail.com>
2024-04-11 15:23:04 -0500

    style login page and update the google sign in button

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -1,23 +1,75 @@
-import { StyleSheet } from "react-native";
 import SignInWithAppleButton from "@/components/auth/SignInWithAppleButton";
 import SignInWithGoogleButton from "@/components/auth/SignInWithGoogleButton";
-import { Platform, Text, View } from 'react-native';
+import { Platform, Text, View, StyleSheet } from 'react-native';
 
 export default function Login() {
     if (Platform.OS === 'ios') {
         return (
             <View style={styles.container}>
-                <Text style={styles.title}>ExpoBase: The free & open source React Native SaaS template</Text>
-                <SignInWithAppleButton />
-                <SignInWithGoogleButton />
+                <View>
+                    <Text style={styles.title}>
+                        ExpoBase{'\n'}
+                    </Text>
+                    <View style={styles.line}></View>
+                    <Text style={styles.paragraph}>
+                        {'\n'}
+                        This open-source template provides a solid foundation for developers to swiftly prototype, iterate, and launch their app.
+                        {'\n'}{'\n'}Tools:
+                    </Text>
+                    <Text style={styles.toolList}>
+                        {"\n"}⚡ Expo for mobile development
+                        {"\n"}🔒 Authentication with Supabase (Apple and Google Auth)
+                        {"\n"}🔐 AuthContext for handling authentication state
+                        {"\n"}⚛️ React Native for building native apps using React
+                        {"\n"}🔥 Type checking with TypeScript
+                        {"\n"}📁 File-based routing with Expo Router
+                        {"\n"}📏 Linter with ESLint
+                        {"\n"}💖 Code Formatter with Prettier
+                        {"\n"}🦺 Unit Testing with Jest and React Testing Library
+                        {"\n"}💡 Absolute Imports using @ prefix
+                        {"\n"}{"\n"}
+                    </Text>
+                </View>
+                <View style={styles.line}></View>
+                <View style={styles.buttonContainer}>
+                    <SignInWithAppleButton />
+                    <Text>{"\n"}</Text>
+                    <SignInWithGoogleButton />
+                </View>
             </View>
         );
     }
     else {
         return (
             <View style={styles.container}>
-                <Text style={styles.title}>ExpoBase: The free & open source React Native SaaS template</Text>
-                <SignInWithGoogleButton />
+                <View>
+                    <Text style={styles.title}>
+                        ExpoBase{'\n'}
+                    </Text>
+                    <View style={styles.line}></View>
+                    <Text style={styles.paragraph}>
+                        {'\n'}
+                        This open-source template provides a solid foundation for developers to swiftly prototype, iterate, and launch their app.
+                        {'\n'}{'\n'}Tools:
+                    </Text>
+                    <Text style={styles.toolList}>
+                        {"\n"}⚡ Expo for mobile development
+                        {"\n"}🔒 Authentication with Supabase (Apple and Google Auth)
+                        {"\n"}🔐 AuthContext for handling authentication state
+                        {"\n"}⚛️ React Native for building native apps using React
+                        {"\n"}🔥 Type checking with TypeScript
+                        {"\n"}📁 File-based routing with Expo Router
+                        {"\n"}📏 Linter with ESLint
+                        {"\n"}💖 Code Formatter with Prettier
+                        {"\n"}🦺 Unit Testing with Jest and React Testing Library
+                        {"\n"}💡 Absolute Imports using @ prefix
+                        {"\n"}{"\n"}
+                    </Text>
+                </View>
+                <View style={styles.line}></View>
+                <View style={styles.buttonContainer}>
+                    <SignInWithGoogleButton />
+                </View>
             </View>
         );
     }
@@ -26,16 +78,28 @@ export default function Login() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: "center",
         justifyContent: "center",
+        padding: 20,
     },
     title: {
-        fontSize: 20,
+        fontSize: 40,
         fontWeight: "bold",
+        textAlign: 'left',
     },
-    separator: {
-        marginVertical: 30,
+    toolList: {
+        fontSize: 16,
+    },
+    paragraph: {
+        fontSize: 24,
+    },
+    line: {
         height: 1,
-        width: "80%",
+        backgroundColor: 'black',
+        width: '100%',
+    },
+    buttonContainer: {
+        marginTop: 20,
+        flexDirection: 'column',
+        alignItems: 'center',
     },
 });
--- src/components/auth/SignInWithGoogleButton.tsx
+++ src/components/auth/SignInWithGoogleButton.tsx
@@ -1,10 +1,39 @@
-import { Button } from "react-native";
+import { Text, StyleSheet, Pressable } from 'react-native';
 import { performGoogleOAuth } from "@/config/authHandler";
 import { useRouter } from 'expo-router';
+import { useState } from 'react';
 
 export default function SignInWithGoogleButton() {
   const router = useRouter();
+  const [isPressed, setIsPressed] = useState(false);
+  const handlePressIn = () => {
+    setIsPressed(true);
+  };
+
+  const handlePressOut = () => {
+    setIsPressed(false);
+  };
+  
   return (
-    <Button onPress={() => performGoogleOAuth(router)} title="Sign in with Google" />
+    <Pressable  onPressIn={handlePressIn} onPressOut={handlePressOut} onPress={() => performGoogleOAuth(router)} style={styles.button}>
+      <Text style={[styles.text,{ color: isPressed ? 'grey' : 'white' }]}>Sign in with Google</Text>
+    </Pressable>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'black',
+    color: 'white',
+    borderRadius: 5,
+    width: 200,
+    height: 64
+  },
+  text: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 18,
+    lineHeight: 64,
+    fontWeight: '600'
+  }
+});
