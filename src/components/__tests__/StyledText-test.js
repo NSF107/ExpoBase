95e07820aee16eeba4650d8529271dccef3e4546
NSF107 <126739555+NSF107@users.noreply.github.com>
2024-04-12 21:35:39 -0500

    fix testing example so it is passing (#4)
    Co-authored-by: Nati <fettsoccer10@gmail.com>

--- src/components/__tests__/StyledText-test.js
+++ src/components/__tests__/StyledText-test.js
@@ -1,10 +1,9 @@
 import * as React from "react";
 import renderer from "react-test-renderer";
+import { MonoText } from "@/components/StyledText";
 
-import { MonoText } from "@/StyledText";
+it('has correct style', () => {
+    const styledText = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
 
-it(`renders correctly`, () => {
-    const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
-
-    expect(tree).toMatchSnapshot();
+    expect(styledText).toMatchSnapshot();
 });
