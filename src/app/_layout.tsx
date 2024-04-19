ed7b94df183e75c905eb4dbcbd3f6ae8825bcb14
Nati <fettsoccer10@gmail.com>
2024-04-19 16:35:23 -0500

    add boilerplate code

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,39 @@
-import { Slot } from "expo-router";
-
+import { Slot, useNavigationContainerRef  } from "expo-router";
+import React from 'react';
 import AuthProvider from "@/context/AuthProvider";
+import * as Sentry from '@sentry/react-native';
+// Construct a new instrumentation instance. This is needed to communicate between the integration and React
+const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
 
-export default function Root() {
-    // Set up the auth context and render our layout inside of it.
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  integrations: [
+    new Sentry.ReactNativeTracing({
+      // Pass instrumentation to be used as `routingInstrumentation`
+      routingInstrumentation,
+      // ...
+    }),
+  ],
+});
+
+function RootLayout() {
+    // Capture the NavigationContainer ref and register it with the instrumentation.
+    const ref = useNavigationContainerRef();
+
+    React.useEffect(() => {
+        if (ref) {
+            routingInstrumentation.registerNavigationContainer(ref);
+        }
+    }, [ref]);
+
     return (
+        // Set up the auth context and render our layout inside of it.
         <AuthProvider>
             <Slot />
         </AuthProvider>
     );
 }
+
+// Wrap the Root Layout route component with `Sentry.wrap` to capture gesture info and profiling data.
+export default Sentry.wrap(RootLayout);
